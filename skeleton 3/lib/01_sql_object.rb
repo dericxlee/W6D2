d7760c9c5b28d5ed8db4c26802bc5f8875d1ed6a
Deric <dericleee@gmail.com>
2023-03-07 16:48:55 -0800

    all

--- skeleton 3/lib/01_sql_object.rb
+++ skeleton 3/lib/01_sql_object.rb
@@ -6,36 +6,18 @@ require 'active_support/inflector'
 class SQLObject
   def self.columns
     # ...
-    # @column ||= DBConnection::execute2(<<-SQL)
-    #   SELECT
-    #     *
-    #   FROM
-    #     #{table_name}
-    # SQL
-
-    DBConnection::execute(<<-SQL)
+    @column ||= DBConnection::execute2(<<-SQL)
       SELECT
         *
       FROM
         #{table_name}
     SQL
 
-
-    # return @column.first.map {|ele| ele.to_sym}
+    return @column.first.map {|ele| ele.to_sym}
   end
 
   def self.finalize!
     @attributes = {}
-
-    # columns.each do |column|
-    #   define_method(column) do
-    #     instance_variable_get "@#{column}"
-    #   end
-
-    #   define_method("#{column}=") do |val|
-    #     instance_variable_set "@#{column}", val
-    #   end
-    # end
     
     @columns = DBConnection::execute(<<-SQL)
       SELECT
